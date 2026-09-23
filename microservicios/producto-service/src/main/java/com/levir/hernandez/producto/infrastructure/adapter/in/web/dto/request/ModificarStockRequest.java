f6c0bbc11006997c7a23fe5d715ed8fe7a452db3
Levir Hernandez <levir.her@gmail.com>
2026-09-23 10:30:00 -0500

    producto-service infraestructura y mensajeria

---
+++ microservicios/producto-service/src/main/java/com/levir/hernandez/producto/infrastructure/adapter/in/web/dto/request/ModificarStockRequest.java
@@ -0,0 +1,13 @@
+package com.levir.hernandez.producto.infrastructure.adapter.in.web.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ModificarStockRequest(
+        @Schema(example = "50")
+        @NotNull(message = "El stock es obligatorio")
+        @PositiveOrZero(message = "El stock no puede ser negativo")
+        Integer stock)
+{
+}
